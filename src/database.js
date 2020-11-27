3977a500d8f57eba0f4aafa92faf033e3622e022
Renan Miranda <renanlmiranda@gmail.com>
2020-11-26 21:29:57 -0300

    hot fix

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://${process.env.DB_HOST}:27017/${process.env.DB_NAME}`, {
+mongoose.connect(process.env.MONGO_URL, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
